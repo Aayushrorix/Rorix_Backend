eab4da5d3b7b0a58de1870e8e76b757764857e1c
Aayushrorix <167325766+Aayushrorix@users.noreply.github.com>
2024-07-02 18:39:33 +0530

    Update settings.py
    r

--- ems/ems/settings.py
+++ ems/ems/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-0t^*61fqqx$kneqg_5v(fc7=x))tk4t4dsx^s=m8@pfj&t2(d+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["192.168.1.27",'localhost','127.0.0.1']
+ALLOWED_HOSTS = ["*","192.168.1.27",'localhost','127.0.0.1']
 
 
 # Application definition
